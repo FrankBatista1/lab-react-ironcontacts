39d39edf85fac5b305d8ff336abb12d193065043
FrankBatista1 <fb53901@gmail.com>
2021-09-07 22:04:44 -0400

    Iteration 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from './contacts.json'
+import { useState } from 'react'
 
 function App() {
+  const [contact, stateContact] = useState(contacts)
+  const filteredContacts = contact.filter((contact,index) => {
+    return index < 5
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header>IronContacts</header>
+      <table>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+        </tr>
+        <tr>
+          <td><img style= {{width: 100}} src={contact[0].pictureUrl}></img></td>
+          <td>{contact[0].name}</td>
+          <td>{contact[0].popularity}</td>
+        </tr>
+        <tr>
+          <td><img style= {{width: 100}} src={contact[1].pictureUrl}></img></td>
+          <td>{contact[1].name}</td>
+          <td>{contact[1].popularity}</td>
+        </tr>
+        <tr>
+          <td><img style= {{width: 100}} src={contact[2].pictureUrl}></img></td>
+          <td>{contact[2].name}</td>
+          <td>{contact[2].popularity}</td>
+        </tr>
+        <tr>
+          <td><img style= {{width: 100}} src={contact[3].pictureUrl}></img></td>
+          <td>{contact[3].name}</td>
+          <td>{contact[3].popularity}</td>
+        </tr>
+        <tr>
+          <td><img style= {{width: 100}} src={contact[4].pictureUrl}></img></td>
+          <td>{contact[4].name}</td>
+          <td>{contact[4].popularity}</td>
+        </tr>
+      </table>
     </div>
-  );
+  )
 }
-
 export default App;
