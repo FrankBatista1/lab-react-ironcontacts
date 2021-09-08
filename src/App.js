ac7b6e4fd7a9484a02d87148bb542c6089d916f8
FrankBatista1 <fb53901@gmail.com>
2021-09-08 10:37:26 -0400

    The buttoms works one time

--- src/App.js
+++ src/App.js
@@ -10,22 +10,27 @@ const newFilteredContacts2 = JSON.parse(JSON.stringify(filteredContacts))
 
 function App() {
   const [state, setState] = useState(filteredContacts)
-  
-  
 
   const addRandomContact = () => {
-    newFilteredContacts2.push(contacts[10])
-    console.log(newFilteredContacts2)
+    const newName = Math.floor(Math.random() * contacts.length);
+    newFilteredContacts2.push(contacts[newName]);
+    console.log(newFilteredContacts2);
     return setState(newFilteredContacts2);
   }
   
+  
+
   const sortTheContactsAlfabetically = () => {
+   
     const newArray = newFilteredContacts.sort((a,b) => a.popularity - b.popularity)
     return setState(newArray)
   }
 
-  const removeContacs = () => {
-    return newFilteredContacts2.slice()
+  const removeContacts = (id) => {
+    const filteredMovies3 = newFilteredContacts2.filter(contact => {
+      return contact.id !== id;
+    });
+    setState(filteredMovies3);
   }
 
   console.log(state)
@@ -33,9 +38,8 @@ function App() {
   return (
     <div className="App">
     <header>IronContacts</header>
-    <button onClick={addRandomContact}>Add a random contact</button>
-    <button onClick={sortTheContactsAlfabetically}>Order Alfabetically</button>
-    <button onClick={removeContacs}>Remove contact</button>
+    <button onClick={() => addRandomContact()}>Add a random contact</button>
+    <button onClick={() => sortTheContactsAlfabetically()}>Order Alfabetically</button>
       <table>
         <tr>
           <th>Picture</th>
@@ -47,13 +51,13 @@ function App() {
         </tr>
         {state.map((contact) => {
           return (
-            <tr>
+            <tr key={contact.id}>
               <td><img style= {{width: 100}} src={contact.pictureUrl} alt="pic"></img></td>
               <td>{contact.name}</td>
               <td>{contact.popularity}</td>
               <td>{contact.wonOscar ? 'Won Oscar' : 'No oscar'}</td>
               <td>{contact.wonEmmy ? 'Won Emmy' : 'No Emmy'}</td>
-              <td><button>Delete</button></td>
+              <button onClick={() => removeContacts(contact.id)}>Delete</button>
             </tr>
           )
         })}
