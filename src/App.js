7be4e1a7c27de82a90f0405087df29580656fbc9
FrankBatista1 <fb53901@gmail.com>
2021-09-08 09:00:01 -0400

    Missing the delete function

--- src/App.js
+++ src/App.js
@@ -5,24 +5,27 @@ import { useState } from 'react'
 const filteredContacts = contacts.filter((contact,index) => {
   return index < 5
 })
+const newFilteredContacts = JSON.parse(JSON.stringify(filteredContacts))
+const newFilteredContacts2 = JSON.parse(JSON.stringify(filteredContacts))
 
 function App() {
   const [state, setState] = useState(filteredContacts)
   
-  const newContcat = contacts[10]
+  
 
   const addRandomContact = () => {
-    const newArrayOfContacts = filteredContacts.push(newContcat)
-    return setState(newArrayOfContacts);
+    newFilteredContacts2.push(contacts[10])
+    console.log(newFilteredContacts2)
+    return setState(newFilteredContacts2);
   }
   
   const sortTheContactsAlfabetically = () => {
-    const newArray = filteredContacts.sort((a,b) => a.popularity - b.popularity)
+    const newArray = newFilteredContacts.sort((a,b) => a.popularity - b.popularity)
     return setState(newArray)
   }
 
   const removeContacs = () => {
-    return 1
+    return newFilteredContacts2.slice()
   }
 
   console.log(state)
@@ -40,15 +43,17 @@ function App() {
           <th>Popularity</th>
           <th>Won an Oscar</th>
           <th>Won an Emmy</th>
+          <th>Delete</th>
         </tr>
         {state.map((contact) => {
           return (
             <tr>
-              <td><img style= {{width: 100}} src={contact.pictureUrl}></img></td>
+              <td><img style= {{width: 100}} src={contact.pictureUrl} alt="pic"></img></td>
               <td>{contact.name}</td>
               <td>{contact.popularity}</td>
               <td>{contact.wonOscar ? 'Won Oscar' : 'No oscar'}</td>
               <td>{contact.wonEmmy ? 'Won Emmy' : 'No Emmy'}</td>
+              <td><button>Delete</button></td>
             </tr>
           )
         })}
